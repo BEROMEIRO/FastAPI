9fbeaf6e415a1f82c65f1e2e3f7ddfa54994ea01
BEROMEIRO <gabriel.romeiro2@gmail.com>
2025-07-07 15:45:08 -0300

    configs.py secao06 finalizada

--- secao06/core/configs.py
+++ secao06/core/configs.py
@@ -6,3 +6,21 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI Project"
     API_V1_STR: str = "/api/v1"
     DB_URL: str = "postgresql+asyncpg://gabriel:romeirosato@127.0.0.1:5432/faculdade3"
+    DBBaseModel = declarative_base
+
+    JWT_SECRET: str = ''
+    '''
+    import secrets
+    token: str = secrets.token_urlsafe(32)
+    '''
+    ALGORITHM: str = 'HS256'
+    # 60 minutos * 24 horas * 7 dias => 1 semana
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+
+
+    class Config:
+        case_sensitive = True
+
+
+settings: Settings = Settings()
+
