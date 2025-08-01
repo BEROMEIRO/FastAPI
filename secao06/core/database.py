1a44ce281008812583b425be63cc759cd8bf6309
BEROMEIRO <gabriel.romeiro2@gmail.com>
2025-08-01 13:31:13 -0300

    ajustando o database.py

--- secao06/core/database.py
+++ secao06/core/database.py
@@ -1,9 +1,7 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine,create_async_engine
 
-
 from secao06.core.configs import settings
-
 
 engine: AsyncEngine = create_async_engine(settings.DB_URL)
 
