a46d0ca0c7a7ff497cdc321b51f825654abe0f11
BEROMEIRO <gabriel.romeiro2@gmail.com>
2025-08-04 12:00:51 -0300

    ajustando database

--- secao06/core/database.py
+++ secao06/core/database.py
@@ -5,6 +5,7 @@ from secao06.core.configs import settings
 
 engine: AsyncEngine = create_async_engine(settings.DB_URL)
 
+
 Session: AsyncSession = sessionmaker(
     bind=engine,
     class_=AsyncSession,
