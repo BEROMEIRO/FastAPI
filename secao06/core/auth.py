0ec8279b3be136a9caf57f0a359338c257671356
BEROMEIRO <gabriel.romeiro2@gmail.com>
2025-08-06 10:54:53 -0300

    ajustando o auth.py

---
+++ secao06/core/auth.py
@@ -0,0 +1,9 @@
+from pytz import timezone
+from typing import Optional, List
+from datetime import datetime, timedelta
+
+from fastapi.security import OAuth2PasswordBearer
+
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from jose import JWTError, jwt  
