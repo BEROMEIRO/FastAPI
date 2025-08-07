20967b5252f08c370da593479b6947ac102117dc
BEROMEIRO <gabriel.romeiro2@gmail.com>
2025-08-07 19:57:51 -0300

    ajustando auth

--- secao06/core/auth.py
+++ secao06/core/auth.py
@@ -5,5 +5,6 @@ from datetime import datetime, timedelta
 from fastapi.security import OAuth2PasswordBearer
 
 from sqlalchemy.future import select
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from jose import JWTError, jwt  
