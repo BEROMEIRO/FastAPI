0f219b0c05f48a11dd7422e20cedb69353b8e5b1
BEROMEIRO <gabriel.romeiro2@gmail.com>
2025-08-26 17:05:41 -0300

    configs + auth ajustado

--- secao06/core/configs.py
+++ secao06/core/configs.py
@@ -9,13 +9,7 @@ class Settings(BaseSettings):
     DBBaseModel = declarative_base
 
     JWT_SECRET: str = 'LfDKcVPc2g9bZ5GbWfcjg1gFcZc4jKmbkrlnv3GCTyw'
-    '''
-    no terminal, abra o python e digite a sequencia abaixo:
-    import secrets
-    token: str = secrets.token_urlsafe(32)
-    token
-    o token irá ser fornecido após o 'token' ser digitado só no terminal 
-    '''
+
     ALGORITHM: str = 'HS256'
     # 60 minutos * 24 horas * 7 dias => 1 semana
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
